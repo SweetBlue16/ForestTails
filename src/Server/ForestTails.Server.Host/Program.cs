1f634073649b615c157c3dae999af5b7eef8d676
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-12-20 19:46:36 -0600

    Added email and notification services to DI container

--- src/Server/ForestTails.Server.Host/Program.cs
+++ src/Server/ForestTails.Server.Host/Program.cs
@@ -1,5 +1,7 @@
 ﻿using CoreWCF.Configuration;
 using ForestTails.Server.Data;
+using ForestTails.Server.Logic.Config;
+using ForestTails.Server.Logic.Utils;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -12,6 +14,14 @@ builder.Host.UseSerilog((ctx, lc) => lc
 builder.Services.AddDbContextFactory<ForestTailsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
+
+// TODO: Dependency injection for data layer
+
+builder.Services.AddSingleton<ServiceExecutor>();
+builder.Services.AddTransient<IEmailService, EmailService>();
+builder.Services.AddTransient<INotificationService, NotificationService>();
+
 builder.Services.AddServiceModelServices();
 builder.Services.AddServiceModelMetadata();
 
@@ -22,5 +32,7 @@ app.UseServiceModel(serviceBuilder =>
     var binding = new CoreWCF.NetTcpBinding();
     binding.Security.Mode = CoreWCF.SecurityMode.None;
 });
+
+Log.Information("Starting ForestTails Server Host...");
 
 app.Run();
