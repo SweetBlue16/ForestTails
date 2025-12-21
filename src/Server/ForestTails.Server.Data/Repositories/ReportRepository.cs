7aef9eff9f7ea7e07fb0966b0a593a48c0280afb
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-12-21 16:19:30 -0600

    Implemented ReportRepository and registered data services

--- src/Server/ForestTails.Server.Data/Repositories/ReportRepository.cs
+++ src/Server/ForestTails.Server.Data/Repositories/ReportRepository.cs
@@ -1,4 +1,6 @@
 ﻿using ForestTails.Server.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ForestTails.Server.Data.Repositories
 {
@@ -7,5 +9,51 @@ namespace ForestTails.Server.Data.Repositories
         Task CreateReportAsync(Report report);
         Task<List<Report>> GetPendingReportsAsync();
         Task MarkAsResolvedAsync(int reportId);
+    }
+
+    public class ReportRepository : BaseRepository<Report>, IReportRepository
+    {
+        public ReportRepository(IDbContextFactory<ForestTailsDbContext> contextFactory,
+        ILogger<ReportRepository> logger) : base(contextFactory, logger) {}
+
+        public async Task CreateReportAsync(Report report)
+        {
+            await ExecuteSafeAsync(async (context) =>
+            {
+                report.CreatedAt = DateTime.UtcNow;
+                report.IsResolved = false;
+
+                await context.AddAsync(report);
+                await context.SaveChangesAsync();
+            },
+            nameof(CreateReportAsync));
+        }
+
+        public async Task<List<Report>> GetPendingReportsAsync()
+        {
+            return await ExecuteSafeAsync(async (context) =>
+            {
+                var list = await context.Reports
+                    .AsNoTracking()
+                    .Include(r => r.Reporter)
+                    .Include(r => r.Reported)
+                    .Where(r => !r.IsResolved)
+                    .OrderBy(r => r.CreatedAt)
+                    .ToListAsync();
+                return list ?? new List<Report>();
+            },
+            nameof(GetPendingReportsAsync));
+        }
+
+        public async Task MarkAsResolvedAsync(int reportId)
+        {
+            await ExecuteSafeAsync(async (context) =>
+            {
+                await context.Reports
+                    .Where(r => r.Id == reportId)
+                    .ExecuteUpdateAsync(s => s.SetProperty(r => r.IsResolved, true));
+            },
+            nameof(MarkAsResolvedAsync));
+        }
     }
 }
--- src/Server/ForestTails.Server.Host/Program.cs
+++ src/Server/ForestTails.Server.Host/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CoreWCF.Configuration;
 using ForestTails.Server.Data;
+using ForestTails.Server.Data.Repositories;
 using ForestTails.Server.Logic.Config;
 using ForestTails.Server.Logic.Utils;
 using Microsoft.EntityFrameworkCore;
@@ -7,16 +8,32 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Information("Configuring ForestTails Server Host...");
+
 builder.Host.UseSerilog((ctx, lc) => lc
     .WriteTo.Console()
     .ReadFrom.Configuration(ctx.Configuration));
 
+Log.Information("Serilog configured.");
+Log.Information("Starting SQL Server Database...");
+
 builder.Services.AddDbContextFactory<ForestTailsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+Log.Information("Configuring SMTP Service...");
+
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
 
-// TODO: Dependency injection for data layer
+Log.Information("SMTP Service configured.");
+Log.Information("Starting ForestTails Services...");
+
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IVerificationCodeRepository, VerificationCodeRepository>();
+builder.Services.AddSingleton<IFriendRepository, FriendRepository>();
+builder.Services.AddSingleton<ICosmeticRepository, CosmeticRepository>();
+builder.Services.AddSingleton<ISanctionRepository, SanctionRepository>();
+builder.Services.AddSingleton<IStatisticsRepository, StatisticsRepository>();
+builder.Services.AddSingleton<IReportRepository, ReportRepository>();
 
 builder.Services.AddSingleton<ServiceExecutor>();
 builder.Services.AddTransient<IEmailService, EmailService>();
@@ -24,6 +41,8 @@ builder.Services.AddTransient<INotificationService, NotificationService>();
 
 builder.Services.AddServiceModelServices();
 builder.Services.AddServiceModelMetadata();
+
+Log.Information("ForestTails Services started.");
 
 var app = builder.Build();
 
