ed48b8947f6ebaf32ae7240c1b36ec6e41fb7913
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-12-21 16:01:51 -0600

    Implemented StatisticsRepository with async methods

--- src/Server/ForestTails.Server.Data/Repositories/StatisticsRepository.cs
+++ src/Server/ForestTails.Server.Data/Repositories/StatisticsRepository.cs
@@ -1,4 +1,6 @@
 ﻿using ForestTails.Server.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ForestTails.Server.Data.Repositories
 {
@@ -7,5 +9,57 @@ namespace ForestTails.Server.Data.Repositories
         Task<PlayerStatistics> GetStatisticsByUserIdAsync(int userId);
         Task UpdateStatisticsAsync(PlayerStatistics statistics);
         Task<List<PlayerStatistics>> GetTopWinsAsync(int count);
+    }
+
+    public class StatisticsRepository : BaseRepository<PlayerStatistics>, IStatisticsRepository
+    {
+        public StatisticsRepository(IDbContextFactory<ForestTailsDbContext> contextFactory,
+        ILogger<StatisticsRepository> logger) : base(contextFactory, logger) {}
+
+        public async Task<PlayerStatistics> GetStatisticsByUserIdAsync(int userId)
+        {
+            return await ExecuteSafeAsync(async (context) =>
+            {
+                var stats = await context.PlayerStatistics
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(s => s.UserId == userId);
+                return stats ?? new PlayerStatistics { UserId = userId };
+            },
+            nameof(GetStatisticsByUserIdAsync));
+        }
+
+        public async Task<List<PlayerStatistics>> GetTopWinsAsync(int count)
+        {
+            return await ExecuteSafeAsync(async (context) =>
+            {
+                var list = await context.PlayerStatistics
+                    .AsNoTracking()
+                    .Include(s => s.User)
+                    .OrderByDescending(s => s.Wins)
+                    .ThenByDescending(s => s.GlobalPoints)
+                    .Take(count)
+                    .ToListAsync();
+                return list ?? new List<PlayerStatistics>();
+            },
+            nameof(GetTopWinsAsync));
+        }
+
+        public async Task UpdateStatisticsAsync(PlayerStatistics statistics)
+        {
+            await ExecuteSafeAsync(async (context) =>
+            {
+                var exists = await context.PlayerStatistics.AnyAsync(s => s.UserId == statistics.UserId);
+                if (exists)
+                {
+                    context.PlayerStatistics.Update(statistics);
+                }
+                else
+                {
+                    await context.PlayerStatistics.AddAsync(statistics);
+                }
+                await context.SaveChangesAsync();
+            },
+            nameof(UpdateStatisticsAsync));
+        }
     }
 }
